c1635c71ecf371044ae9adec112b2df64d12e529
Elias <eliasq_@hotmail.com>
2023-03-29 20:24:39 -0300

    Exemplo de plot com matplotlib

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import matplotlib.pyplot as plt
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    squares = [1, 4, 9, 16, 25]
+
+    fig, ax = plt.subplots()
+    ax.plot(squares)
+
+    plt.show()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
