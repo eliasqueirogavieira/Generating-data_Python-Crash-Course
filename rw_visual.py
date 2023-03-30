41f40c4b518dec5a9fb4d10862b498596add2d47
Elias <eliasq_@hotmail.com>
2023-03-29 22:43:20 -0300

    PreExercise

--- rw_visual.py
+++ rw_visual.py
@@ -7,20 +7,20 @@ while True:
 
     #Make a random walk.
 
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     #Plot the points in the walk.
 
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     ax.set_aspect('equal')
 
     #Emphasize the first and last points.
-    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax.scatter(0, 0, c='green', edgecolors='none', s=5)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=5)
 
     #Remove the axes.
     ax.get_xaxis().set_visible(False)
