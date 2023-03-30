2d2c0f0ecc934764aac076906db3f52caac8c6fc
Elias <eliasq_@hotmail.com>
2023-03-30 00:44:02 -0300

    D6 and D10

---
+++ die_visual_d6d10.py
@@ -0,0 +1,29 @@
+from die import Die
+import plotly.express as px
+
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = f"Results of Rolling a D6 and a D10 50000 times."
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+# Further customize chart
+fig.update_layout(xaxis_dtick=1)
+fig.write_html('die_visual_d6d10.html')
+fig.show()
