45c5b76d7d2d51524972c6596e644048d477cb83
Elias <eliasq_@hotmail.com>
2023-03-30 01:27:48 -0300

    Exercise 15-9

--- multdice.py
+++ multdice.py
@@ -7,20 +7,17 @@ die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
-    result = die_1.roll() * die_2.roll()
-    results.append(result)
+
+results = [die_1.roll() * die_2.roll() for roll_num in range(1000)]
 
 # Analyze the results.
 frequencies = []
 max_result = die_1.num_sides * die_2.num_sides
 poss_results = range(1, max_result+1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in poss_results]
 
 # Visualize the results.
-title = f"Results of Rolling Three D6s 1000 times."
+title = f"Results of Rolling Two D6s 1000 times."
 labels = {'x': 'Result', 'y': 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 # Further customize chart
