34fd6da4cd418c3be8441d0553a630195b0ecf55
Elias <eliasq_@hotmail.com>
2023-03-29 21:43:30 -0300

    Scatter plot and design

--- main.py
+++ main.py
@@ -7,21 +7,24 @@
 import matplotlib.pyplot as plt
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    input_values = [1, 2, 3, 4, 5]
-    squares = [1, 4, 9, 16, 25]
+    x_values = range(1,1001)
+    y_values = [x**2 for x in x_values]
 
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
-    ax.plot(input_values, squares, linewidth=3)
+    ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
     #Set chart title and label axes.
-    ax.set_title("Square numbers", fontsize=20)
+    ax.set_title("Square numbers", fontsize=24)
     ax.set_xlabel("Value", fontsize=14)
     ax.set_ylabel("Square of Value", fontsize=14)
 
-    #Set size of tick labels.
-    ax.tick_params(axis='both', labelsize=14)
+    #Set the range for each axis.
+    ax.axis([0, 1100, 0, 1100000])
+    ax.ticklabel_format(style='plain')
 
+    plt.savefig('squares_plot.png', bbox_inches='tight')
     plt.show()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
