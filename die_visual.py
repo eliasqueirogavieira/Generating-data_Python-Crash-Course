30c6ae1da4d0f5b2b7285d61d5c015458f46d635
Elias <eliasq_@hotmail.com>
2023-03-30 00:34:28 -0300

    Using Plotly to show data

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,5 @@
 from die import Die
+import plotly.express as px
 
 # Create a D6.
 die= Die()
@@ -9,4 +10,15 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+title = f"Results of rolling one D{die.num_sides} 1000 times."
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
